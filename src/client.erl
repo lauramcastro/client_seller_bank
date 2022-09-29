1fbbed168b86269459522c32dce7583cbc601a03
Laura M. Castro <lcastro@udc.es>
2022-09-29 11:05:35 +0200

    Client and seller implementation (pending testing).

--- src/client.erl
+++ src/client.erl
@@ -6,30 +6,46 @@
 -behaviour(gen_server).
 
 %% Custom API
--export([start/0, pay/1]).
+-export([start/0, pay/2]).
 
 %% GenServer API
 -export([start_link/1]).
 %% GenServer callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
+-define(TITLE, "Novecento").
+-define(LIMIT, 50).
+-define(CARD_NUMBER, "0000000000000000").
+
+-spec start()->'#seller!title<string>.seller?price<integer>.@(seller!ok.seller?pay<integer>.seller!card<string>.&(seller?date<string>.end,seller?ko.end),seller!ko.end)'.
 start() ->
     start_link(?MODULE).
-
-pay(Price) ->
-    gen_server:handle_call(?MODULE, {pay, Price}).
 
 start_link(Name) ->
     gen_server:start_link({local, Name}, ?MODULE, [], []).
 
+pay(Who, Price) ->
+    gen_server:handle_call(?MODULE, {Who, pay, Price}).
+
 init(_Args) ->
-    {ok, []}.
+    seller:title(?TITLE),
+    {ok, ?TITLE}.
 
 handle_call(stop, _From, State) ->
     {stop, normal, stopped, State};
 
-handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
+handle_call({seller, price,  Price}, _From, ?TITLE) when Price < ?LIMIT ->
+    {reply, ok, {?TITLE, Price}};
+handle_call({seller, price,  Price}, _From, {?TITLE, Price})  ->
+    {reply, ko, []};
+
+handle_call({seller, pay, Price}, _From, {?TITLE, Price}) ->
+    case seller:card_details({client, card, ?CARD_NUMBER}) of
+        {seller, date, _Date} ->
+           {reply, ok, []};
+        {seller, ko} ->
+            {reply, ko, []}
+        end.
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
--- src/seller.erl
+++ src/seller.erl
@@ -13,6 +13,8 @@
 %% GenServer callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
+-define(PRICES, #{"Novecento" => 25}).
+
 start() ->
     start_link(?MODULE).
 
@@ -25,12 +27,29 @@ init(_Args) ->
 handle_call(stop, _From, State) ->
     {stop, normal, stopped, State};
 
+handle_call({client, title, Title}, _From, []) ->
+    Price = maps:get(Title, ?PRICES),
+    {reply, {?MODULE, price, Price}, {client, Title, Price}};
+
+handle_call({client, ok}, _From, {client, Title, Price}) ->
+    bank:price(Price),
+    bank ! {seller, start_delegation, seller, self()},
+    {noreply, {client, Title, Price}};
+
+handle_call({client, ko}, _From, {client, _Title, _Price}) ->
+    {noreply, []};
+
+handle_call({bank, ok}, _From, {client, _Title, _Price}) ->
+    {reply, {seller, date, "22 ottobre 2022"}, []};
+
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
+handle_info({bank, end_delegation}, {client, Title, Price}) ->
+    {noreply, {client, Title, Price}};
 handle_info(_Info, State) ->
     {noreply, State}.
 
